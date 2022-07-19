b900739b17071bed6b7d9582809f547274d523c7
BazaroZero <finatov05@gmail.com>
2022-07-19 16:35:38 +0300

    feat(users): add regex validator on phone field

--- shering/users/models.py
+++ shering/users/models.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta, timezone
 
 from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.core.validators import RegexValidator
 from django.db import models
 
 from .managers import UserManager
@@ -9,7 +10,11 @@ from .utils import send_sms
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    phone = models.CharField(unique=True, max_length=20)
+    phone_regex = RegexValidator(
+        regex=r"^\+?1?\d{8,15}$",
+        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.",
+    )
+    phone = models.CharField(unique=True, max_length=17, validators=[phone_regex])
     code_created_at = models.DateTimeField(auto_now=True)
     is_staff = models.BooleanField(default=False)
     USERNAME_FIELD = "phone"
